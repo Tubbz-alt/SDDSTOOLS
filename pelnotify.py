879c5fe45585ace4cc7985c7a2fba6025fafaba8
Joel Frederico <joelfred@slac.stanford.edu>
2011-05-19 11:07:19 -0700

    Updated pelnotify to have a default deck, change log file

--- pelnotify.py
+++ pelnotify.py
@@ -5,21 +5,23 @@ from subprocess import call
 import os
 
 
-def runElegant(elefile,cores,notify,email,verbose):
+def runElegant(elefile,cores,notify,email,log,verbose):
 	if verbose: print("Email notification to " + email + "...\nRunning " + elefile + " with Pelegant...")
 	options=""
 	if notify: options=options + " -N"
 	if email!=None: options=options + " -u " + email
-	maincommand="bsub -a mympi -q beamphysics -oo run.log -n " + str(cores) + options + " Pelegant " + elefile
+	maincommand="bsub -a mympi -q beamphysics -oo " + log + " -n " + str(cores) + options + " Pelegant " + elefile
+	# print maincommand
 	call(shlex.split(maincommand))
 
-class myAction(argparse.Action):
+class note_address(argparse.Action):
 	def __call__(self, parser, namespace, values, option_string=None):
 		if values==None:
 			try:
 				values=os.environ['NOTIFY_EMAIL']
+				namespace.notify=True
 			except:
-				raise argparse.ArgumentError(self,'unable to retrieve e-mail, no argument given')
+				raise argparse.ArgumentError(self,'expected one argument ($NOTIFY_EMAIL not set)')
 		setattr(namespace, self.dest, values)
 
 if __name__ == '__main__':
@@ -31,21 +33,15 @@ if __name__ == '__main__':
 	parser=argparse.ArgumentParser(description='Process command line.')
 	parser.add_argument('-V',action='version',version='%(prog)s v0.1')
 	parser.add_argument('-v','--verbose', action='store_true', help='Verbose mode.')
-	parser.add_argument('deck',action='store',
+	parser.add_argument('-l','--log',default='run.log',help='Log file output.')
+	parser.add_argument('deck',action='store',nargs='?',default='facet.ele',
 			help='Elegant input deck.')
 	parser.add_argument('-n','--number', action='store', type=int, default=10,
 			help='Number of cores to use.')
 	parser.add_argument('-N','--notify', action='store_true',default=False,
 			help='Enable notification by email.  (Default goes to SLAC email account.)')
-	# parser.add_argument('-e','--email', nargs='?',const=email,
-	#                 help='Overrides which email account to send to.  If enabled, overrides notification flag.\n' \
-	#                 'Searches for email in environment variable $NOTIFY_EMAIL.')
-	parser.add_argument('-e','--email', nargs='?', action=myAction,
+	parser.add_argument('-e','--email', nargs='?', action=note_address,
 			help='Overrides which email account to send to.  If enabled, overrides notification flag.\n' \
 			'Searches for email in environment variable $NOTIFY_EMAIL.')
 	arg=parser.parse_args()
-	# if arg.email==None:
-	#         raise argparse.ArgumentError('default','expected at least one argument')
-	# else:
-	#         arg.notify=True
-	runElegant(arg.deck,arg.number,arg.notify,arg.email,arg.verbose)
+	runElegant(arg.deck,arg.number,arg.notify,arg.email,arg.log,arg.verbose)
