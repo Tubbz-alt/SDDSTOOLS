9a131c261da2cd8b23db5e93fe686637b98837a9
Mike Litos <litos@slac.stanford.edu>
2011-07-18 11:11:35 -0700

    Touched up gen_par.sh and read_par.sh scripts a bit.

--- read_par.sh
+++ read_par.sh
@@ -12,7 +12,31 @@
 # NOTE: 'value' column can contain double or string.
 #---------------------------------------------------
 
-sdds2plaindata $1 $2 \
+# usage
+if ! [ $1 ]; then
+    echo "usage:"
+    echo "read_par.sh input.par [output.dat]"
+    exit 1
+fi
+
+# define variables from arguments
+input=$1
+
+if [ $2 ]; then
+    output=$2
+else
+    output=${input%.par}
+    output=$output.dat
+fi
+
+# don't overwrite existing output file
+if [ -e $output ]; then
+    echo "Error: output file $output aleardy exists."
+    exit 1
+fi
+
+# generate plain text file from parameter file
+sdds2plaindata $input $output \
 -col=ElementName -col=ElementParameter \
 -col=ParameterValueString -col=ParameterMode \
 "-separator= " -noRowCount
