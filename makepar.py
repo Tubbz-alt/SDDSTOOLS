74d46eae120ba721ab36def5bb5713a139eaad80
Joel Frederico <joelfred@slac.stanford.edu>
2011-07-26 15:10:43 -0700

    Updated makepar to account for an occurence column.

--- makepar.py
+++ makepar.py
@@ -5,35 +5,67 @@ import shlex
 from subprocess import call
 
 def makepar(inputf,outputf,verbose):
+	# Specify delimiters for input, intermediate files.
 	csv.register_dialect('par',delimiter=',',skipinitialspace=True)
 	csv.register_dialect('parout',delimiter=' ',skipinitialspace=True)
 	
-	f=open(inputf)
+	# Open for reading with unidentified EOL character
+	f=open(inputf,'rU')
 	rdr=csv.reader(f,dialect='par')
+
+	# Extract header
 	header=list(rdr.next())
 	rdrlist=list(rdr)
-	len(rdrlist)
 	
 	par=open(outputf + ".tmp",'w')
 	wrtr=csv.writer(par,dialect='parout')
 
-	print len(header)
-	print rdrlist
-	for i in range(1,len(header)+1):
-		print i
-		for j in rdrlist:
-			print j[i].strip()
-			print "hey"
-			row=[j[0].strip(), header[i-1].strip(), j[i].strip()]
-			print row
-			# print header[i]
-			# print i
+	# Find the index for "Occurence" if it exists
+	try:
+		occ=header.index("Occurence")
+	except ValueError:
+		occ=0
+	
+	# Inform if there is occurence data.
+	if verbose:
+		if occ==0:
+			print "No occurence data."
+		else:
+			print "Occurence at index", occ
+
+	print header
+	for j in rdrlist:
+		for i in range(1,len(header)):
+			# Prevents treating occurence as a parameter.
+			if i==occ:
+				continue
+
+			# If occurence exists, use it.
+			if occ>0:
+				row=[j[0].strip(), j[occ], header[i].strip(), j[i].strip()]
+			else:
+				row=[j[0].strip(), header[i].strip(), j[i].strip()]
+
+			if verbose:
+				print row
+
+			# Write to file to be parsed by plaindata2sdds
 			wrtr.writerow(row)
 	
 	f.close()
 	par.close()
-	command="plaindata2sdds " + outputf + ".tmp " + outputf + " -col=ElementName,string -col=ElementParameter,string -col=ParameterValue,double -noRowCount -outputMode=binary \"-separator= \""
-	print command
+
+	# Modify command to include occurrence data.
+	if occ>0:
+		occstr="-col=ElementOccurence,double "
+	else:
+		occstr=""
+
+	# Change intermediate file to .par file
+	command="plaindata2sdds " + outputf + ".tmp " + outputf + " -col=ElementName,string " + occstr +  "-col=ElementParameter,string -col=ParameterValue,double -noRowCount -outputMode=binary \"-separator= \""
+	if verbose:
+		print "Command is:"
+		print command
 	call(shlex.split(command))
 
 
