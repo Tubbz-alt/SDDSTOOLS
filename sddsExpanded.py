e005ec4b70f786b8f979b871625bde6d21d177d0
Joel Frederico <joelfred@slac.stanford.edu>
2012-08-23 14:10:07 -0700

    Updated sddsExpanded with more functions.

--- sddsExpanded.py
+++ sddsExpanded.py
@@ -1,15 +1,20 @@
+# Extends default sdds python toolkit.
 import sdds
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Converts sdds to an array.
 def sdds2array(instance,var):
 	return np.array(instance.columnData[instance.columnName.index(var)][0])
 
+# 2-D histogram
 def hist2d(x,y,bins=10):
 	h,xe,ye=np.histogram2d(x,y,bins=bins)
 	extent=[xe[0],xe[-1],ye[-1],ye[0]]
+	# fig=plt.figure()
 	fig=plt.gcf()
 	ax=fig.gca()
+	ax.clear()
 	ax.imshow(h.transpose(),extent=extent)
 
 
@@ -23,6 +28,7 @@ def hist2d(x,y,bins=10):
 	# fig.show()
 	# return fig
 
+# Show and configure any figure
 def showfig(fig):
 	ax=fig.gca()
 	# Swap y axis if needed
@@ -35,5 +41,38 @@ def showfig(fig):
 	ax.set_aspect('auto')
 	fig.show()
 
+# Add labels to figures
 def addlabels(xl,yl,tl):
-	pass
+	fig=plt.gcf()
+	fig.suptitle(tl)
+	plt.xlabel(xl)
+	plt.ylabel(yl)
+	showfig(fig)
+
+# Create a histogram from an sdds file.
+def sddshist2d(filename,array,bins=10):
+	this=sddsload(filename)
+
+	for i,label in enumerate(array):
+		if label=='z':
+			t=sdds2array(this,'t')
+			dt=t-np.mean(t)
+			temp=dt*299792458
+		elif label=='dp':
+			p=sdds2array(this,'p')
+			temp=(p-np.mean(p))/np.mean(p)
+		else:
+			temp=sdds2array(this,label)
+		if i==0:
+			x=temp
+		else:
+			y=temp
+	
+	hist2d(x,y,bins)
+	addlabels(array[0],array[1],'')
+
+# Load an sdds file.
+def sddsload(filename):
+	this=sdds.SDDS(0)
+	this.load(filename)
+	return this
