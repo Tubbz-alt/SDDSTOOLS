2e4929740fd78b79a6b6b233aa0e6cfafeeb3621
Joel Frederico <joelfred@slac.stanford.edu>
2012-11-02 13:49:23 -0700

    Updated fithist, init for sddsToolkit

--- sddsToolkit/__init__.py
+++ sddsToolkit/__init__.py
@@ -3,5 +3,7 @@
 from loadpage import loadpage
 from sdds2array import sdds2array
 from fitbunch import fitbunch
+from fithist import fithist
+# from scanpar import scanpar
 
 import printpage
--- sddsToolkit/fithist.py
+++ sddsToolkit/fithist.py
@@ -2,20 +2,32 @@ import numpy as np
 import scipy.optimize as spopt
 import matplotlib.pyplot as plt
 
-def fithist(res,
+def fithist(bin_edges,counts,
 		fitfunc=None,
 		p0=None
 		):
 	"""Finds a fit given histogram data."""
 
-	# Define fitfunc if not specified
+	# Separation between hist points
+	delta=bin_edges[1]-bin_edges[0]
+	# Find coordinates - bin_edges returns edges, want center
+	coord=bin_edges[0:len(counts)]+delta
+
+	# If there's no fitfunc, assume gaussian distribution
 	if fitfunc == None:
+		# If there's no guess, come up with one
+		if p0 == None:
+			# Find average as if all counts are at the center of their bin.
+			mean = sum(coord*counts)/sum(counts)
+			# Find std dev as if all counts are at the center of their bin.
+			std = np.sqrt(sum( counts*((coord-mean)**2) )/sum(counts))
+			# print std
+			p0 = [max(counts), std, mean]
+			print p0
 		fitfunc=lambda p, x: p[0]*np.exp(-np.square(x-p[2])/(2*np.square(p[1])))
 	errfunc = lambda p, x, y: fitfunc(p,x) - y
-
-	delta=res[1][1]-res[1][0]
-	coord=res[1][0:bins]+delta
-	counts=res[0]
 	
-	p0 = [max(counts), np.std(x), np.mean(x)]
 	p1, success = spopt.leastsq(errfunc,p0[:],args=(coord,counts))
+
+	return p1,success
+
