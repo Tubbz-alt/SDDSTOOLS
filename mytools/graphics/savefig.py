409c4454f1bac74e405a7ad980a3904d6b272525
Joel Frederico <joelfred@slac.stanford.edu>
2014-10-22 10:47:30 -0700

    Added savefig

--- mytools/graphics/savefig.py
+++ mytools/graphics/savefig.py
@@ -14,8 +14,13 @@ def savefig(filename,path="figs",fig=None,ext='eps',**kwargs):
 
 	filename       = ''.join([path,'/',filename])
 	final_filename = '{}.{}'.format(filename,ext).replace(" ","").replace("\n","")
+	final_filename = os.path.abspath(final_filename)
+	final_dir = os.path.dirname(final_filename)
+	
+	if not os.path.exists(final_dir):
+		os.makedirs(final_dir)
 
-	if fig != None:
+	if fig is not None:
 		fig.savefig(final_filename,bbox_inches='tight',**kwargs)
 	else:
 		plt.savefig(final_filename,bbox_inches='tight',**kwargs)
